7328f1889eeb0177cc39cc37c0c08f7a7f87dcb6
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-04-08 11:36:03 -0700

    changing to using SQLAlchemy, Marshmellow with Flask

--- api/index.py
+++ api/index.py
@@ -1,6 +1,22 @@
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+app = Flask(__name__)
+
+class Restaurant(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(50))
+  address = db.Column(db.String(100))
+  website = db.Column(db.String(100))
+  
+  def __repr__(self):
+    return '<Restaurant: %s>' % self.name
 
 @app.route("/api/healthchecker", methods=["GET"])
 def healthchecker():
@@ -16,17 +32,17 @@ def add_restaurant():
   data = request.get_json()
   return 201
 
-@app.router("/restaurants/<int:id>", methods=["GET"])
+@app.route("/restaurants/<int:id>", methods=["GET"])
 def get_restaurant(id):
   return {"status": "success", "message": f"This will return a single restaurant with ID of {id}"}
 
-@app.router("/restaurants/<int:id>", methods=["PATCH"])
+@app.route("/restaurants/<int:id>", methods=["PATCH"])
 def patch_restaurant(id):
   return {"status": "success", "message": f"This will edit a single restaurant with ID of {id}"}
 
-@app.router("/restaurants/<ing:id>", methods=["DELETE"])
+@app.route("/restaurants/<ing:id>", methods=["DELETE"])
 def delete_restaurant(id):
   return {"status": "success", "message": f"This will edit a single restaurant with ID of {id}"}
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
