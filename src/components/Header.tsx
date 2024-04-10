036b89ef59ecad8b13c795afd7529ec0f5a8d73a
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-04-10 14:21:39 -0700

    refactor bounded, button, header and icon (fka logo)

---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+
+import { Link } from "react-router-dom"
+import Icon from "./Icon";
+import Bounded from "./Bounded";
+
+export default function Header(props: HeaderProps) {
+
+  return (
+  <Bounded as="header" className="py-4 md:py-6 lg:py-8 ">
+    <div className="flex gap-4 items-center justify-between sm:flex-row flex-col">
+    <Link to="/">
+    <Icon/>
+    </Link>
+
+    <nav>
+      <ul className="flex ">
+        {props.navigation.map(({link, label})=>(
+          <li key={label}>
+          <Link to={link} className="p-3">{label}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+    </div>
+    </Bounded>
+  )
+}
+
+interface HeaderProps {
+  navigation: []
+}
