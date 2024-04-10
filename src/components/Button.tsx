2de7e98fd59f5cfbb458db43bf8348ab9e26cba4
Brianca Knight <briancaknight@gmail.com>
2024-04-10 10:58:49 -0700

    update restaurantList, map, and header slices

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,19 @@
 import { PrismicNextLink, PrismicNextLinkProps } from "@prismicio/next";
 import clsx from "clsx";
 
-export default function Button ({
-  className, 
+export default function Button({
+  className,
   ...restProps
-}: PrismicNextLinkProps){
+}: PrismicNextLinkProps) {
+
   return (
+      <PrismicNextLink
 
-    <PrismicNextLink
-  
-     className={clsx("black w-fit bg-rose-950 transition-color duration-200 ease-in-out py-3 px-12 rounded-full font-display text-white font-bold text-base tracking-wide mb-8 md:mb-10 border-amber-50 border-2", className)}
+        className={clsx("black w-fit bg-rose-950 transition-color duration-200 ease-in-out py-3 px-12 rounded-full font-display text-white font-bold text-base tracking-wide mb-2 md:mb-3 border-amber-50 border-2 mt-2", className)}
 
-     {...restProps}
-     />
+        {...restProps}
+      />
+
    
   )
 }
--- src/slices/Header/index.tsx
+++ src/slices/Header/index.tsx
@@ -2,6 +2,7 @@ import { Content } from "@prismicio/client";
 import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Bounded from "@/components/Bounded";
 import Heading from "@/components/Heading"
+import Button from "@/components/Button";
 
 const components: JSXMapSerializer = {
   heading1: ({ children }) => (
@@ -11,7 +12,7 @@ const components: JSXMapSerializer = {
       className="md:mb-8 mb-4 mt-12 first:mt-0 last:mb-0">
       {children}</Heading>
   ),
-  paragraph: ({ children }) => (<p className="text-lg font-body max-w-md text-center">{children}</p>)
+  paragraph: ({ children }) => (<p className="text-lg font-body max-w-md text-center mb-0">{children}</p>)
 }
 
 /**
@@ -24,6 +25,8 @@ export type HeaderProps = SliceComponentProps<Content.HeaderSlice>;
  */
 const Header = ({ slice }: HeaderProps): JSX.Element => {
   return (
+    <>
+    {slice.variation === "default" && (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
@@ -33,7 +36,22 @@ const Header = ({ slice }: HeaderProps): JSX.Element => {
       <PrismicRichText field={slice.primary.body} components={components} />
       </div>
     </Bounded>
-    
+    )}
+       {slice.variation === "withButton" && (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <div className="grid grid-cols-1 place-items-center txt-center">
+      <PrismicRichText field={slice.primary.title} components={components}/>
+      <PrismicRichText field={slice.primary.body} components={components} />
+      <Button field={slice.primary.button_link} >
+        <>{slice.primary.button_text}</>
+      </Button>
+      </div>
+    </Bounded>
+    )}
+    </>
   );
 };
 
