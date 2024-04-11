0e4df9f963b305d92a09aaab0d7022ada6c8ba05
Kim Robinson <kimmykokonut@gmail.com>
2024-04-11 10:44:21 -0700

    update db and display restaurant info on restlist page

--- src/components/Restaurant.tsx
+++ src/components/Restaurant.tsx
@@ -4,20 +4,22 @@ interface RestaurantProps {
   address: string;
   imageUrl: string;
   website: string;
+  typeName: string;
+  typeAmount: string;
 }
 
-const Restaurant: React.FC<RestaurantProps> = ({ name, address, website, imageUrl }) => {
+const Restaurant: React.FC<RestaurantProps> = ({ name, address, website, imageUrl, typeName, typeAmount }) => {
 
   return (
     <div className="p-4 max-w-sm">
       <div className="max-w-4xl m-2 shadow-xl md:px-12 px-4 py-12 grid place-items-center rounded-lg bg-gradient-to-br from-rose-800 via-slate to-gray-500">
-
-        <a href={website}><img src={imageUrl} alt="Restaurant logo" height="256px" width="256px" />
-          <br />
-          <h3>{name}</h3>
-          <p>{address}</p></a>
-        <p>link to directions?</p>
-        <p>type of service fee</p>
+        <a href={website} target="_blank" rel="noopener noreferrer"><img src={imageUrl} alt="Restaurant logo" height="256px" width="256px" />
+        <br />
+        <h3>{name}</h3></a>
+        <p>{address}</p>
+        <br />
+        <p><a href={`https://www.google.com/maps/dir/?api=1&destination=${address}`} target="_blank" rel="noopener noreferrer">Directions</a></p>
+        <p>{typeName} {isNaN(parseFloat(typeAmount)) ? typeAmount : `${Math.round(parseFloat(typeAmount) * 100)}%`}</p>
       </div>
     </div>
   );
