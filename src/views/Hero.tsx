f5a44bd014170c52aca47d96c9a2371e87707f6e
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-04-11 10:09:29 -0700

    update logo, work on hero

--- src/views/Hero.tsx
+++ src/views/Hero.tsx
@@ -1,6 +1,3 @@
-import { Content } from "@prismicio/client";
-import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
-import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Bounded from "../components/Bounded";
 import Button from "../components/Button";
 import Heading from "../components/Heading";
@@ -16,63 +13,25 @@ const components: JSXMapSerializer = {
   paragraph: ({ children }) => (<p className="text-xl font-body mb-4 md:mb-8 max-w-md">{children}</p>)
 }
 
-/**
- * Props for `Hero`.
- */
-export type HeroProps = SliceComponentProps<Content.HeroSlice>;
-
-/**
- * Component for "Hero" Slices.
- */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
     <>
-      {slice.variation === "default" && (
         <Bounded
           data-slice-type={slice.slice_type}
           data-slice-variation={slice.variation}
         >
-
-          <div className="grid grid-cols-1 place-items-center txt-center">
-
-            <PrismicRichText field={slice.primary.heading}
-              components={components} />
-            <PrismicRichText field={slice.primary.body} components={components} />
-            <Button to={slice.primary.button_link} className="mb-8 md:mb-10">
-              <>{slice.primary.button_text}</>
-            </Button>
-            <PrismicNextImage
-              field={slice.primary.image}
-              className="dop-shaddow-xl max-w-4xl w-full" />
-
-          </div>
-
-        </Bounded>
-      )}
-
-      {slice.variation === "horizontal" && (
-        <Bounded
-          data-slice-type={slice.slice_type}
-          data-slice-variation={slice.variation}
-        >
-
           <div className="grid grid-cols-1 md:grid-cols-2 place-items-center">
             <div className="grid grid-rows-[1fr,auto,auto] gap-8 h-fit">
-              <PrismicRichText field={slice.primary.heading}
-                components={components} />
-              <PrismicRichText field={slice.primary.body} components={components} />
-              <Button to={slice.primary.button_link} className="mb-8 md:mb-10">
-                <>{slice.primary.button_text}</>
-              </Button>
+              <p>TEXT TEXT TEXT</p>
+              <p>TEXT TEXT TEXT</p>
+              <Button 
+                to="/"
+                className="mb-8 md:mb-10" />
             </div>
-            <PrismicNextImage
-              field={slice.primary.image}
+            <img src=""
               className="dop-shaddow-xl max-w-4xl w-full" />
-
           </div>
-
         </Bounded>
-      )}
     </>
   );
 
