0ddfafef01649fd8f931d28b35269c4135f2e032
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-04-11 10:18:21 -0700

    fix hero a bit

--- src/views/Hero.tsx
+++ src/views/Hero.tsx
@@ -2,35 +2,29 @@ import Bounded from "../components/Bounded";
 import Button from "../components/Button";
 import Heading from "../components/Heading";
 
-const components: JSXMapSerializer = {
-  heading1: ({ children }) => (
-    <Heading
-      as="h1"
-      size="xl"
-      className="md:mb-8 mb-4 mt-12 first:mt-0 last:mb-0">
-      {children}</Heading>
-  ),
-  paragraph: ({ children }) => (<p className="text-xl font-body mb-4 md:mb-8 max-w-md">{children}</p>)
-}
-
-const Hero = ({ slice }: HeroProps): JSX.Element => {
+const Hero = () => {
   return (
     <>
         <Bounded
-          data-slice-type={slice.slice_type}
-          data-slice-variation={slice.variation}
         >
+        <Heading
+      as="h1"
+      size="xl"
+      className="md:mb-8 mb-4 mt-12 first:mt-0 last:mb-0"
+      children={<p>OK</p>} >
+        </Heading>
           <div className="grid grid-cols-1 md:grid-cols-2 place-items-center">
             <div className="grid grid-rows-[1fr,auto,auto] gap-8 h-fit">
-              <p>TEXT TEXT TEXT</p>
-              <p>TEXT TEXT TEXT</p>
+              <p className="text-xl font-body mb-4 md:mb-8 max-w-md">TEXT TEXT TEXT</p>
+              <p className="text-xl font-body mb-4 md:mb-8 max-w-md">TEXT TEXT TEXT</p>
               <Button 
                 to="/"
                 className="mb-8 md:mb-10" />
             </div>
-            <img src=""
+            <img src="https://live.staticflickr.com/3036/2869854667_7304d2568e_c_d.jpg"
               className="dop-shaddow-xl max-w-4xl w-full" />
           </div>
+          
         </Bounded>
     </>
   );
