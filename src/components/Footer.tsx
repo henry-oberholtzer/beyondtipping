586178041f4dd5c9458d039b9fd72b9df32398d4
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-04-10 21:50:58 -0700

    tailwind working, header, heading and footer should be OK now, Hero view in progress conversion

---
+++ src/components/Footer.tsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'
+import Bounded from "./Bounded";
+import Icon from "./Icon";
+
+export default function Footer(props: HeaderFooterProps) {
+
+  return (
+  <Bounded as="footer">
+    <div className="flex sm:flex-row flex-col justify-between items-center gap-6">
+    <Link to="/"><Icon/></Link>
+
+<p className="text-xs">©{new Date().getFullYear()} beyondtipping</p>
+
+      <ul className="flex">
+        {props.navigation.map(({link, label})=>(
+          <li key={label}>
+          <Link to={link} className="p-3">{label}</Link>
+          </li>
+        ))}
+      </ul>
+      </div>
+</Bounded>
+  )
+}
