62e8d78a067f39746b86eea8f854e4a856a247f0
noahkise <noah@kisefamily.com>
2024-04-11 16:46:50 -0700

    add confirmation page

---
+++ src/components/Confirmation.tsx
@@ -0,0 +1,7 @@
+export const Confirmation = () => {
+    return (
+        <>
+            <h1>Thank you for your submission! It will be reviewed by an administrator.</h1>
+        </>
+    )
+} 
--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,7 @@ import RestaurantDetail from './components/RestaurantDetail.tsx';
 import Bios from './views/Bio.tsx';
 import { Form } from './components/Form.tsx';
 import WhyTipFree from './components/WhyTipFree.tsx';
+import { Confirmation } from './components/Confirmation.tsx';
 
 
 const navigation: NavigationDir[] = [
@@ -90,6 +91,10 @@ const router = createBrowserRouter([
       {
         path: "form",
         element: <Form />
+      },
+      {
+        path: "confirmation",
+        element: <Confirmation />
       },
       {
         path: "restaurants",
