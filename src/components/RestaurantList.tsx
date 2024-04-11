1b5753ad20dfeb114e7b9e0d9f47873ad4a22cb5
Kim Robinson <kimmykokonut@gmail.com>
2024-04-11 10:44:21 -0700

    update db and display restaurant info on restlist page

--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -1,7 +1,7 @@
+import { useEffect, useState } from "react";
 import Restaurant from "./Restaurant";
-import { Link } from "react-router-dom";
 import { useLoaderData } from 'react-router'
-
+import { getTypes } from "../api_helper";
 
 type Restaurant = {
   name: string;
@@ -9,12 +9,28 @@ type Restaurant = {
   address: string;
   website: string;
   imageUrl: string;
+  type_id: number;
+}
+interface TypeData {
+  id: number;
+  name: string;
+  amount: string;
 }
 
 type RestaurantItemList = Restaurant[]
 
 const RestaurantList: React.FC = () => {
   const restaurants = useLoaderData() as RestaurantItemList
+  const [types, setTypes] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchTypes = async () => {
+      const allTypes = await getTypes();
+      const typesMap = allTypes.reduce((map: { [key: number]: TypeData }, type: TypeData) => ({ ...map, [type.id]: type }), {});
+      setTypes(restaurants.map((restaurant) => typesMap[restaurant.type_id]));
+    };
+    fetchTypes();
+  }, [restaurants]);
 
   return (
     <>
@@ -23,8 +39,7 @@ const RestaurantList: React.FC = () => {
       </div>
     
     <div className="flex flex-wrap justify-center mt-1">
-      {restaurants.map((restaurant) => (
-        // <Link to={`restaurants/${restaurant.id}`} key={restaurant.id}>
+      {restaurants.map((restaurant, index) => (
           <Restaurant
             key={restaurant.id}
             name={restaurant.name}
@@ -32,6 +47,8 @@ const RestaurantList: React.FC = () => {
             imageUrl={restaurant.imageUrl}
             website={restaurant.website}
             id={restaurant.id}
+            typeName={types[index]?.name}
+            typeAmount={types[index]?.amount}
           />
           // </Link>
       ))}
