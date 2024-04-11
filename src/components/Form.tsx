e38323b0b5ffd063e78329242403b03e2046865d
noahkise <noah@kisefamily.com>
2024-04-11 13:42:25 -0700

    form working

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,11 @@
+import { createRestaurant } from "../api_helper";
+import { FormEvent } from 'react';
+
 export const Form = () => {
 
     const apiKey = import.meta.env.VITE_API_KEY;
 
-    const makeCall = async (address) => {
+    const makeCall = async (address: string) => {
         const requestOptions = {
             method: 'GET',
         };
@@ -10,11 +13,8 @@ export const Form = () => {
             const response = await fetch(`https://api.geoapify.com/v1/geocode/search?text=${address}&apiKey=${apiKey}`, requestOptions)
             if (response.ok) {
                 const responseData = await response.json();
-                console.log(responseData);
                 const lat = responseData.features[0].properties.lat;
                 const lon = responseData.features[0].properties.lon;
-                console.log(lat);
-                console.log(lon);
                 return [lat, lon];
             } else {
                 throw new Error('Failed to fetch');
@@ -26,51 +26,58 @@ export const Form = () => {
     }
 
 
-    const handleSubmit = async (event) => {
-        event.preventDefault(); // Prevents the default form submission behavior
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
 
-        // Extracting values from input fields
-        const name = document.getElementById("restaurantName").value;
-        const address = document.getElementById("restaurantAddress").value;
-        const website = document.getElementById("restaurantWebsite").value; // Extracting website value
-        const imageUrl = document.getElementById("restaurantImageURL").value; // Extracting imageURL value
-        const type_id = document.getElementById("restaurantType").value;
+        const nameElement = document.getElementById("restaurantName") as HTMLInputElement;
+        const addressElement = document.getElementById("restaurantAddress") as HTMLInputElement;
+        const websiteElement = document.getElementById("restaurantWebsite") as HTMLInputElement;
+        const imageURLElement = document.getElementById("restaurantImageURL") as HTMLInputElement;
+        const typeElement = document.getElementById("restaurantType") as HTMLSelectElement;
 
-        // Making the API call to get latitude and longitude
-        const [lat, lon] = await makeCall(address);
+        if (nameElement && addressElement && websiteElement && imageURLElement && typeElement) {
+            const name = nameElement.value;
+            const address = addressElement.value;
+            const website = websiteElement.value;
+            const imageUrl = imageURLElement.value;
+            const type_id = typeElement.value;
 
-        // Creating the restaurantData object with latitude and longitude
-        const restaurantData = {
-            name: name,
-            address: address,
-            website: website, // Adding website to the object
-            imageUrl: imageUrl, // Adding imageURL to the object
-            type_id: parseInt(type_id), // Converting type_id to an integer
-            latitude: lat,  // Adding latitude to the object
-            longitude: lon  // Adding longitude to the object
-        };
+            const [lat, lon] = await makeCall(address);
 
-        // Do whatever you want with the restaurantData object, for example, log it to console
-        console.log(restaurantData);
+            const restaurantData = {
+                name: name,
+                address: address,
+                website: website,
+                imageUrl: imageUrl,
+                type_id: parseInt(type_id),
+                latitude: lat,
+                longitude: lon
+            };
+            createRestaurant(restaurantData);
+        } else {
+            console.error("One or more elements not found.");
+        }
     };
 
     return (
         <>
-            <h1>FORM</h1>
             <form id="newRestaurantForm" onSubmit={handleSubmit}>
-                <input type="text" id="restaurantName" name="name" placeholder="Restaurant Name" />
-                <input type="text" id="restaurantAddress" name="address" placeholder="Restaurant Address" />
-                <input type="text" id="restaurantWebsite" name="website" placeholder="Restaurant Website" />
-                <input type="text" id="restaurantImageURL" name="imageURL" placeholder="Restaurant Image URL" />
-                <p>Restaurant Type:</p>
-                <select id="restaurantType" name="type">
-                    <option value="1">Flat Fee</option>
-                    <option value="2">18% Service Fee</option>
-                    <option value="3">20% Service Fee</option>
-                    <option value="4">22% Service Fee</option>
-                    <option value="6">Other % Service Fee</option>
-                </select>
-                <button type="submit" id="restaurantSubmit">Submit</button>
+                <div id="formInputs">
+                    <h1>add new restaurant</h1>
+                    <input type="text" className="input" id="restaurantName" name="name" placeholder="Restaurant Name" />
+                    <input type="text" className="input" id="restaurantAddress" name="address" placeholder="Restaurant Address" />
+                    <input type="text" className="input" id="restaurantWebsite" name="website" placeholder="Restaurant Website" />
+                    <input type="text" className="input" id="restaurantImageURL" name="imageURL" placeholder="Restaurant Image URL" />
+                    <p>Restaurant Type:</p>
+                    <select className="input" id="restaurantType" name="type">
+                        <option value="1">Flat Fee</option>
+                        <option value="2">18% Service Fee</option>
+                        <option value="3">20% Service Fee</option>
+                        <option value="4">22% Service Fee</option>
+                        <option value="6">Other % Service Fee</option>
+                    </select>
+                    <button type="submit" id="restaurantSubmit">Submit</button>
+                </div>
             </form>
         </>
     );
